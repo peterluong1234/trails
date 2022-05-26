c145c2f9ad8c5c7b6425d3d7c68435071519f4d1
Peter <peterluong1123@gmail.com>
2022-05-26 12:22:34 -0700

    addtrailform updated

--- src/components/AddTrailForm/AddTrailForm.jsx
+++ src/components/AddTrailForm/AddTrailForm.jsx
@@ -1,7 +1,97 @@
 import React, { useState } from 'react';
+import { Button, Form, Grid, Header, Image,  Segment } from 'semantic-ui-react';
 
 export default function AddTrailForm(){
+    const [selectedFile, setSelectedFile] = useState('')
+    const [state, setState] = useState({
+        name: '',
+        start: '',
+        end: '',
+        type: ''
+    })
+
+    function handleFileInput(e){
+        setSelectedFile(e.target.files[0])
+      }
+    
+    
+      function handleChange(e){
+        setState({
+          ...state,
+          [e.target.name]: e.target.value
+        })
+      }
+    
+      function handleSubmit(e){
+        e.preventDefault()
+                 
+        const formData = new FormData()
+        formData.append('photo', selectedFile)
+        formData.append('name', state.name)
+        formData.append('start', state.start)
+        formData.append('end', state.end)
+        formData.append('type', state.type)
+        // Have to submit the form now! We need a function!
+      }
+    
+
     return(
-        <span>Trail Form</span>
+          
+    <Grid textAlign='center' verticalAlign='middle'>
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Segment>
+      
+          <Form  autoComplete="off" onSubmit={handleSubmit}>
+             <Form.Input
+                className="form-control"
+                name="name"
+                value={state.name}
+                placeholder="name"
+                onChange={handleChange}
+                required
+                />   
+            <Form.Input
+                className="form-control"
+                name="start"
+                value={state.start}
+                placeholder="start"
+                onChange={handleChange}
+                required
+            />   
+            <Form.Input
+                className="form-control"
+                name="end"
+                value={state.end}
+                placeholder="end"
+                onChange={handleChange}
+                required
+            />   
+            <Form.Input
+                className="form-control"
+                name="type"
+                value={state.type}
+                placeholder="type"
+                onChange={handleChange}
+                required
+            />   
+            <Form.Input
+              className="form-control"
+              type="file"
+              name="photo"
+              placeholder="upload image"
+              onChange={handleFileInput}
+            />   
+            <Button
+              type="submit"
+              className="btn"
+            >
+              ADD PUPPY
+            </Button>
+          </Form>
+        </Segment>
+    </Grid.Column>
+  </Grid>
+ 
+
     )
 }
