406d5b2f25bbd9004b0d3164ac3f7e52824b6c8c
Peter Luong <peterluong@Peters-MBP.socal.rr.com>
2022-05-26 01:05:23 -0700

    Added loader

---
+++ src/components/AddTrailForm/AddTrailForm.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function AddTrailForm(){
+    return(
+        <span>Trail Form</span>
+    )
+}
--- src/pages/CreateTrailPage/CreateTrailPage.jsx
+++ src/pages/CreateTrailPage/CreateTrailPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import PageHeader from '../../components/Header/Header'
+import AddTrailForm from '../../components/AddTrailForm/AddTrailForm' 
+import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 
-export default function CreateTrailPage(props){
+export default function CreateTrailPage({user, handleLogout}){
+    const [trail, setTrail] = useState({});
+    const [error, setError] = useState("")
+    
+    if (error) {
+        return (
+          <>
+            <PageHeader handleLogout={handleLogout} user={user}/>
+            <ErrorMessage error={error} />;
+          </>
+        );
+      }
+    
+      if (loading) {
+        return (
+          <>
+            <PageHeader handleLogout={handleLogout} user={user}/>
+            <Loading />
+          </>
+        );
+      } 
+    
+
     return (
         <>
-        <span>Trail Page</span>
+        <PageHeader handleLogout={handleLogout} user={user}/>
+        <AddTrailForm />
+
         </>
     )
 }
